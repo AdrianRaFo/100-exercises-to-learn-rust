7fe95b296f8947ee0f959f76085aab932080a9b1
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2024-06-05 23:20:02 +0200

    in progress 6

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -15,7 +15,13 @@ pub fn fibonacci(n: u32) -> u32 {
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut vec = vec![0, 1];
+    
+    for i in 1..=n {
+        vec.push(vec[i as usize] + vec[(i - 1) as usize]);
+    }
+
+    vec[n as usize]
 }
 
 #[cfg(test)]

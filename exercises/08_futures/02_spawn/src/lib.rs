a3ef96f562263e2dfda6e49a4099b618889eb436
AdrianRaFo <15971742+AdrianRaFo@users.noreply.github.com>
2024-07-05 00:36:36 +0200

    complete 7 except outro

--- exercises/08_futures/02_spawn/src/lib.rs
+++ exercises/08_futures/02_spawn/src/lib.rs
@@ -1,10 +1,29 @@
 use tokio::net::TcpListener;
+use tokio::task::JoinHandle;
 
 // TODO: write an echo server that accepts TCP connections on two listeners, concurrently.
 //  Multiple connections (on the same listeners) should be processed concurrently.
 //  The received data should be echoed back to the client.
 pub async fn echoes(first: TcpListener, second: TcpListener) -> Result<(), anyhow::Error> {
-    todo!()
+    fn listen(listener: TcpListener) -> JoinHandle<Result<(), anyhow::Error>> {
+        tokio::spawn(async move {
+            loop {
+                let (mut socket, _) = listener.accept().await?;
+
+                tokio::spawn(async move {
+                    let (mut reader, mut writer) = socket.split();
+                    tokio::io::copy(&mut reader, &mut writer)
+                        .await
+                        .expect("Error copying");
+                });
+            }
+        })
+    }
+
+    let (outcome1, outcome2) = tokio::join!(listen(first), listen(second));
+    outcome1??;
+    outcome2??;
+    Ok(())
 }
 
 #[cfg(test)]
--- exercises/08_futures/03_runtime/src/lib.rs
+++ exercises/08_futures/03_runtime/src/lib.rs
@@ -2,15 +2,37 @@
 //  accept connections on both of them concurrently, and always reply clients by sending
 //  the `Display` representation of the `reply` argument as a response.
 use std::fmt::Display;
+use std::sync::Arc;
 use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
+use tokio::task::JoinHandle;
 
 pub async fn fixed_reply<T>(first: TcpListener, second: TcpListener, reply: T)
 where
     // `T` cannot be cloned. How do you share it between the two server tasks?
     T: Display + Send + Sync + 'static,
 {
-    todo!()
+    let arc_reply = Arc::new(reply);
+    let (_, _) = tokio::join!(
+        listen(first, Arc::clone(&arc_reply)),
+        listen(second, arc_reply)
+    );
+}
+
+async fn listen<T>(listener: TcpListener, reply: Arc<T>) -> JoinHandle<Result<(), anyhow::Error>>
+where
+    // `T` cannot be cloned. How do you share it between the two server tasks?
+    T: Display + Send + Sync + 'static,
+{
+    tokio::spawn(async move {
+        loop {
+            let (mut socket, _) = listener.accept().await.unwrap();
+            let reply_clone = Arc::clone(&reply);
+            tokio::spawn(async move {
+                socket.write_all(format!("{}", reply_clone).as_bytes()).await.unwrap();
+            });
+        }
+    })
 }
 
 #[cfg(test)]
